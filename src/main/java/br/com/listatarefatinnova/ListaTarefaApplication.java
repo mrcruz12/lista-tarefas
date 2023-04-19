4191402ce5ace6a124fd5a2b877d4eda55e57e5b
Matheus Rodrigues da Cruz <38232751+mrcruz12@users.noreply.github.com>
2023-04-18 23:24:18 -0300

    first commit

---
+++ src/main/java/br/com/listatarefatinnova/ListaTarefaApplication.java
@@ -0,0 +1,13 @@
+package br.com.listatarefatinnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaTarefaApplication.class, args);
+	}
+
+}
